4b5382fbed4cfd1e693384842e21c5a7b6ae9a30
Shivamj4112 <101393149+Shivamj4112@users.noreply.github.com>
2023-05-10 17:02:21 +0530

    Add files via upload

---
+++ day3nk.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+	
+	int a;
+	float b;
+	
+	printf("Enter First Number : ");
+	scanf("%d",&a);
+	
+	printf("Enter Second Number : ");
+	scanf("%f",&b);
+	
+	float sum = a+b;
+	      
+	printf("%f",sum);
+	
+	return 0;
+}
